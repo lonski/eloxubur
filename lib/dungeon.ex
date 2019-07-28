99a3407b0405af908119978654a87e2c7e038f32
Michal Lonski <michal.lonski@nokia.com>
2019-07-28 20:23:07 +0200

    extracted dungeon generator parameters to constants

--- lib/dungeon.ex
+++ lib/dungeon.ex
@@ -1,27 +1,38 @@
 defmodule Dungeon do
+  @fill_treshold 0.6
+  @room_chance_percent 10
+  @room_size_min 3
+  @room_size_max 8
+  @corridor_len_min 4
+  @corridor_len_max 10
+
   def generate(width, height) do
     grid = Grid.new(width, height, '#')
     start_pos = {div(width, 2), div(height, 2)}
 
-    add_feature(grid, start_pos, 0.6)
+    add_features(grid, start_pos, @fill_treshold)
   end
 
   def fill_level(grid) do
     max(Grid.count(grid, '.'), 1) / max(Grid.count(grid, '#'), 1)
   end
 
-  defp add_feature(grid, pos, trsh) do
+  defp add_features(grid, pos, trsh) do
     {grid, pos} =
-      case :rand.uniform(10) < 3 do
+      case :rand.uniform(100) < @room_chance_percent do
         true -> try_make_room(grid, pos, random_direction())
         false -> try_make_corridor(grid, pos, random_direction())
       end
 
-    if fill_level(grid) < trsh, do: add_feature(grid, pos, trsh), else: grid
+    if fill_level(grid) < trsh, do: add_features(grid, pos, trsh), else: grid
   end
 
   defp try_make_room(grid, {sx, sy}, {dir_x, dir_y}) do
-    {x_size, y_size} = {random(3, 8), random(3, 8)}
+    {x_size, y_size} = {
+      random(@room_size_min, @room_size_max),
+      random(@room_size_min, @room_size_max)
+    }
+
     {ex, ey} = {sx + dir_x + x_size, sy + dir_y + y_size}
     points = rect_points({sx, sy}, {ex, ey})
 
@@ -32,7 +43,7 @@ defmodule Dungeon do
   end
 
   defp try_make_corridor(grid, {sx, sy}, {dir_x, dir_y}) do
-    len = random(4, 10)
+    len = random(@corridor_len_min, @corridor_len_max)
     {ex, ey} = {sx + dir_x * len, sy + dir_y * len}
     points = rect_points({sx, sy}, {ex, ey})
 
